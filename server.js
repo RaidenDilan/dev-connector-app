5d72be3e9f66432b0cca0b0f3a9c7d29596cd403
RaidenDilan <raiden18@me.com>
2020-05-18 23:59:25 +0100

    Route Files With Express Router

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const mongoose = require('mongoose');
 
 const { dbURI, dbOps, port } = require('./config/environment');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 mongoose
   .connect(dbURI, dbOps)
   .then(() => console.log('MongoDB Connected!'))
   .catch(err => console.log('MongoDB failed to connect!', err));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 app.listen(port, () => console.log(`Server running on port ${ port }`));
