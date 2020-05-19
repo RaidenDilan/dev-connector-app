753cb8f2ba3b50c4f87a38a5ffeab2d052053400
RaidenDilan <raiden18@me.com>
2020-05-19 01:01:46 +0100

    User Registration  Postman

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { dbURI, dbOps, port } = require('./config/environment');
 
@@ -9,6 +10,9 @@ const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 mongoose
   .connect(dbURI, dbOps)
