41a132083340ce03586fb56016526ccc95f3fc9d
RaidenDilan <raiden18@me.com>
2020-06-11 23:24:06 +0100

    Registration The Error Reducer - Final

--- client/src/store/actions/authActions.js
+++ client/src/store/actions/authActions.js
@@ -1,8 +1,15 @@
-import { TEST_DISPATCH } from './types';
-// Register User
-const registerUser = userData => ({
-  type: TEST_DISPATCH,
-  payload: userData
-});
+import axios from 'axios';
+import { GET_ERRORS } from './types';
 
-export default registerUser;
+// Register User
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post('/api/users/register', userData)
+    .then(res => history.push('/login'))
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
