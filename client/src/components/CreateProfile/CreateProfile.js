1fe5dd339190eaaa4359035b0c28c2afdba6bb85
RaidenDilan <raiden18@me.com>
2020-06-17 02:49:33 +0100

    Create Profile Functionality

--- client/src/components/CreateProfile/CreateProfile.js
+++ client/src/components/CreateProfile/CreateProfile.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../Common/TextFieldGroup';
 import TextAreaFieldGroup from '../Common/TextAreaFieldGroup';
 import InputGroup from '../Common/InputGroup';
 import SelectListGroup from '../Common/SelectListGroup';
+import { createProfile } from '../../store/actions/profileActions';
 
 class CreateProfile extends Component {
   constructor(props) {
@@ -32,10 +34,30 @@ class CreateProfile extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) this.setState({ errors: nextProps.errors });
+  }
+
   onSubmit(e) {
     e.preventDefault();
 
-    console.log('[ CreateProfile ] - onSubmit', e);
+    const profileData = {
+      handle: this.state.handle,
+      company: this.state.company,
+      website: this.state.website,
+      location: this.state.location,
+      status: this.state.status,
+      skills: this.state.skills,
+      githubusername: this.state.githubusername,
+      bio: this.state.bio,
+      twitter: this.state.twitter,
+      facebook: this.state.facebook,
+      linkedin: this.state.linkedin,
+      youtube: this.state.youtube,
+      instagram: this.state.instagram
+    };
+
+    this.props.createProfile(profileData, this.props.history);
   }
   
   onChange(e) {
@@ -225,4 +247,4 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(mapStateToProps, {})(CreateProfile);
+export default connect(mapStateToProps, { createProfile })(withRouter(CreateProfile));
