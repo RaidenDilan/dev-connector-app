1be20b9129602893ee55b93d77460f884c7bc5d1
RaidenDilan <raiden18@me.com>
2020-08-14 20:10:45 +0100

    Changed react folder naming convention- tbc

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
 
 import Dashboard from './components/dashboard/Dashboard';
-import CreateProfile from './components/profile/CreateProfile';
+import CreateProfile from './components/profile/createProfile';
 import EditProfile from './components/profile/EditProfile';
 import AddExperience from './components/profile/addCredentials/AddExperience';
 import AddEducation from './components/profile/addCredentials/AddEducation';
@@ -30,7 +30,7 @@ import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
 import Post from './components/posts/post/Post';
 
-import NotFound from './components/NotFound/NotFound';
+import NotFound from './components/notFound/NotFound';
 
 import './App.css';
 
