3f407f23dba575bdbae4864ecb995b2c61a26749
RaidenDilan <raiden18@me.com>
2020-08-15 16:29:02 +0100

    Prepared for deployment

--- server.js
+++ server.js
@@ -13,30 +13,20 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
-// // Serve static assets if in production
-// if (process.env.NODE_ENG === 'production') {
-//   app.use(express.static('client/build')); // Set static folder
-//   app.get('*', (req, res) => res.sendfile(path.resolve(__dirname, 'cleint', 'build', 'index.html')));
-// }
+// Serve static assets if in production
+if (process.env.NODE_ENG === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => res.sendfile(path.resolve(__dirname, 'cleint', 'build', 'index.html')));
+}
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(dbURI, dbOps);
-    console.log("MongoDB is Connected...");
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
-
-connectDB();
-// mongoose
-//   .connect(dbURI, dbOps)
-//   .then(() => console.log('MongoDB Connected!'))
-//   .catch(err => console.log('MongoDB failed to connect!', err));
+mongoose
+  .connect(dbURI, dbOps)
+  .then(() => console.log('MongoDB Connected!'))
+  .catch(err => console.log('MongoDB failed to connect!', err));
 
 // passport middleware
 app.use(passport.initialize());
@@ -46,11 +36,5 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
-
-// Serve static assets if in production
-if (process.env.NODE_ENG === 'production') {
-  app.use(express.static('client/build')); // Set static folder
-  app.get('*', (req, res) => res.sendfile(path.resolve(__dirname, 'cleint', 'build', 'index.html')));
-}
 
 app.listen(port, () => console.log(`Server running on port ${ port }`));
