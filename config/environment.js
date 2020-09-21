fadba7089af7e0a32f3a9995fe33821312110883
RaidenDilan <raiden18@me.com>
2020-09-21 16:17:19 +0100

    Preparing to deploy

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,7 @@
 const port = process.env.PORT || 5000;
 const env = process.env.NODE_ENV || 'development';
 const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@devconnector.s3ubt.mongodb.net/${process.env.DB_NAME}-${env}?retryWrites=true&w=majority`;
+
 const dbOps = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
