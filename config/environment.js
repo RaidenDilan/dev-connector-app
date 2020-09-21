6112ed97bddc8b436017dc01539882766039b6b2
RaidenDilan <raiden18@me.com>
2020-09-21 16:24:27 +0100

    Preparing to deploy

--- config/environment.js
+++ config/environment.js
@@ -1,13 +1,13 @@
 const port = process.env.PORT || 5000;
 const env = process.env.NODE_ENV || 'development';
 const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@devconnector.s3ubt.mongodb.net/${process.env.DB_NAME}-${env}?retryWrites=true&w=majority`;
+const secret = 'secret_dev_key';
 
 const dbOps = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
 };
-const secret = 'secret_dev_key';
 
 module.exports = {
   port,
