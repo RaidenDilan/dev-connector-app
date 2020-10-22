e96a4fdc600eeb92994ad11479c8fb669191b7b4
RaidenDilan <raiden18@me.com>
2020-10-22 15:13:20 +0100

    Deploying to Heroku

--- server.js
+++ server.js
@@ -21,15 +21,15 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Serve static assets if in production
-if (process.env.NODE_ENG === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(compression());
   // app.use(enforce.HTTPS({ trustProtoHeader: true })); // trustProtoHeader => Heroku runs a reverse-proxy
   // Set static folder
   // app.use(express.static('client/build'));
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-    // res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    // res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
