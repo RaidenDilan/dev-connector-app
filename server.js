e4da7d1964f651e24aeaed988b6f2be493cadb1e
RaidenDilan <raiden18@me.com>
2020-10-22 19:51:57 +0100

    Deploying to Heroku

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
-const cors = require('cors');
-const compression = require('compression');
-// const enforce = require('express-sslify');
 
 const { dbURI, dbOps, port } = require('./config/environment');
 
@@ -16,22 +13,17 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
-
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(compression());
-  // app.use(enforce.HTTPS({ trustProtoHeader: true })); // trustProtoHeader => Heroku runs a reverse-proxy
+if (process.env.NODE_ENG === 'production') {
   // Set static folder
-  // app.use(express.static('client/build'));
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    // res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 mongoose
   .connect(dbURI, dbOps)
