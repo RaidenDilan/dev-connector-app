4edb50a6005871941c42ce27606a0c24eb24817b
RaidenDilan <raiden18@me.com>
2020-10-22 04:57:07 +0100

    Updated dev keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@devconnector.s3ubt.mongodb.net/${process.env.DB_NAME}-${env}?retryWrites=true&w=majority`,
+  mongoURI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@devconnector.s3ubt.mongodb.net/${process.env.DB_NAME}-production?retryWrites=true&w=majority`,
   secretOrKey: process.env.SECRET_OR_KEY
 };
